d1f4359a368f0ab69e373bd0e31ee2bb0ab30615
bharatwadekar007-crypto <bharatwadekar007@gmail.com>
2025-09-15 06:54:49 +0530

    Update dependencies.js

--- packages/expo-dev-client/dependencies.js
+++ packages/expo-dev-client/dependencies.js
@@ -1,1 +1,8 @@
 module.exports = {};
+"dependencies": {
+  "react": "18.2.0",
+  "react-native": "0.73.6",
+  "expo": "~50.0.14",
+  "@react-native-picker/picker": "2.7.5",
+  "expo-file-system": "~16.0.9"
+}
